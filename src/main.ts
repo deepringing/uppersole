64ae728bcf99fb2a41121776816081bee935c664
gimhanul <gimhanuly@gmail.com>
2023-04-11 12:04:16 +0900

    SET :: swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaService } from './prisma/prisma.service';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app)
+
+
+  const config = new DocumentBuilder()
+    .setTitle('윈물 API')
+    .setDescription('윈물이 맑아야 아랜물이 말그다')
+    .setVersion('v.0.0.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+
+  await app.listen(3000);
 }
+
 bootstrap();
